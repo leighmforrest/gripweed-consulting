f22d51a8e92365a071dfc44a09e2ca0fe858bf7b
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-11-29 05:33:45 -0500

    inline script added

--- public/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector("#hambuger");
-  const menu = document.querySelector("#menu");
-
-  hamburger.addEventListener("click", ()=> menu.classList.toggle("hidden"))
-});
