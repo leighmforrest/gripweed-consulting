2f29d2e653dd56f0e92d94553c076af51afaf73b
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-12-21 06:19:33 -0500

    improved form validation

---
+++ src/scripts/contactForm.js
@@ -0,0 +1,27 @@
+export function showError(input, message) {
+  const error = input.parentElement.querySelector(".error-message");
+  error.textContent = message;
+  error.classList.remove("hidden");
+
+  input.classList.remove("border-green-500", "border-gray-300");
+  input.classList.add("border-red-500");
+}
+
+export function markValid(input) {
+  const error = input.parentElement.querySelector(".error-message");
+  error.textContent = "";
+  error.classList.add("hidden");
+
+  input.classList.remove("border-red-500", "border-gray-300");
+  input.classList.add("border-green-500");
+}
+
+
+export function clearError(input) {
+  const error = input.parentElement.querySelector(".error-message");
+  error.textContent = "";
+  error.classList.add("hidden");
+
+  input.classList.remove("border-red-500", "border-gray-300");
+  input.classList.add("border-green-500");
+}
